41e059b2162252e887864276e98451ad9d068263
hedgestandart <hedgestandart@gmail.com>
2023-12-25 15:10:33 +0300

    Outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterPreloader } from '@angular/router';
+import { Router,Route } from '@angular/router';
+import { RouterModule } from '@angular/router';
+//import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,  FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'summer';
 }
+ 
---
+++ src/app/dist/app.routes.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+exports.routes = void 0;
+exports.routes = [];
